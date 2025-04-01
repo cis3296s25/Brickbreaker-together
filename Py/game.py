75b1f188a2b033bae4821d719e64847bcecb4c80
Linnan Chen <tus11309@temple.edu>
2025-04-01 17:10:52 -0400

    Multiple game with simple logic, countdown animate

--- Py/game.py
+++ Py/game.py
@@ -6,13 +6,18 @@ from paddle import Paddle
 from ball import Ball
 from brick import Brick
 from ui import UI
+from powerup import PowerUp
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.paddle = Paddle()
         self.ball = Ball(self.paddle)
-        
+        self.balls = [self.ball] #Now supports multiple balls
+        self.powerups = []
+        self.bomb_ready = False
+        self.speed_modifier = 1.0  # Normal speed initially
+        self.slow_until = 0
         # Load game music
         try:
             self.game_music_path = os.path.join('Py', 'audio', 'game_music.mp3')
@@ -66,7 +71,8 @@ class Game:
                     self.waiting_for_start = False
                     self.isGameInProgress = True
                     self.isPaused = False
-                    self.ball.reset(self.paddle)
+                    for ball in self.balls:
+                        ball.reset(self.paddle)
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
@@ -100,29 +106,62 @@ class Game:
 
     def update(self):
         if self.isGameInProgress and not self.isPaused:
-            result = self.ball.move(self.paddle, self.bricks)
-            if result == 10:
-                self.score += 10
-            elif result == -1:
-                self.lives -= 1
-                if self.lives <= 0:
-                    self.game_over()
-                else:
-                    self.ball.reset(self.paddle)
+            for ball in self.balls[:]:
+                result = ball.move(self.paddle, self.bricks, self, self.speed_modifier)
+
+                if result == 10:
+                    self.score += 10
+                    if random.random() < 0.2:
+                        self.powerups.append(PowerUp(ball.rect.centerx, ball.rect.centery))
+
+                elif result == -1:
+                    self.balls.remove(ball)
+                    if len(self.balls) == 0:
+                        self.lives -= 1
+                        if self.lives <= 0:
+                            self.game_over()
+                        else:
+                            new_ball = Ball(self.paddle)
+                            self.balls.append(new_ball)
+
+        # Update powerups
+        for powerup in self.powerups[:]:
+            powerup.update()
+            if powerup.rect.colliderect(self.paddle.rect):
+                powerup.apply(self)
+                self.powerups.remove(powerup)
+            elif powerup.rect.top > HEIGHT:
+                self.powerups.remove(powerup)
+                
+        # Reset speed modifier after slow effect ends
+        if self.speed_modifier < 1.0 and pygame.time.get_ticks() > self.slow_until:
+            self.speed_modifier = 1.0
+
 
     def draw(self):
         self.screen.fill(BACKGROUND_COLOR)
-        
-        # Draw everything using the UI's draw method
-        self.ui.draw(self.screen, self.score, self.lives, self.isPaused)
-        
-        # Draw game elements
-        self.paddle.draw(self.screen)
-        self.ball.draw(self.screen)
+
+        # First draw UI container
+        self.ui.draw_container(self.screen)
+
+        # Then draw bricks and other game elements
         for brick in self.bricks:
             brick.draw(self.screen)
-        
-        # make sure the pause menu is on top
+
+        for powerup in self.powerups:
+            powerup.draw(self.screen)
+
+        self.paddle.draw(self.screen)
+
+        # NOW draw all balls (on top of UI container)
+        for ball in self.balls:
+            ball.draw(self.screen)
+
+        # Finally draw UI elements (score, lives, etc.)
+        self.ui.draw_lives(self.screen, self.lives)
+        self.ui.draw_score(self.screen, self.score)
+        self.ui.draw_pause_button(self.screen, self.isPaused)
+
         if self.isPaused:
             self.ui.pause_menu.draw(self.screen)
 
@@ -161,8 +200,11 @@ class Game:
     def reset_game(self):
         self.lives = 3
         self.score = 0
-        self.ball.reset(self.paddle)
+        self.balls = [Ball(self.paddle)]
         self.create_bricks()
+        self.powerups.clear()
+        self.bomb_ready = False
+        self.slow_until = 0
 
     def run(self):
         clock = pygame.time.Clock()
